863a56622dc341773a91b00ab632ecb766833524
GauravJangidRam <gaurav14jangid@gmail.com>
2024-05-20 15:38:08 +0530

    Responsive

--- src/view/screen/ProductDetails.js
+++ src/view/screen/ProductDetails.js
@@ -1,116 +1,202 @@
-import { Card, Container, Image, Col, Row } from "react-bootstrap"
-import { useLocation, useNavigate } from "react-router-dom"
-import { useEffect, useState } from "react"
-import '../style/AddToCart.css'
+
+// import { Card, Container, Image, Col, Row } from "react-bootstrap"
+// import { useLocation, useNavigate } from "react-router-dom"
+// import { useEffect, useState } from "react"
+// import '../style/AddToCart.css'
+
+// function ProductDetails() {
+//   const loc = useLocation();
+//   const navigate = useNavigate();
+//   const [details1, setDetails] = useState('');
+//   const [selectedSize, setSelectedSize] = useState('');
+//   const [selectedColor, setSelectedColor] = useState('');
+
+//   useEffect(() => {
+//     setDetails(loc.state);
+//   }, [loc.state]);
+
+//   function AddTo() {
+//     navigate('/AddToCart', { state: { ...details1, selectedSize, selectedColor } });
+//   }
+
+//   const BuyNow = () => {
+//     navigate('/AddToCart', { state: { ...details1, selectedSize, selectedColor } });
+//   }
+
+//   return (
+//     <>
+//       <Container>
+//         <div className="heading">
+//           <h1 className="heading-add">Product Details</h1>
+//           <hr className="heading-line" />
+//         </div>
+//         <Row>
+//           <div className="image-section">
+//             <div className="left-img">
+//               <Col>
+//                 {
+//                   details1 && details1.sideimg.map((value, index) => (
+//                     <Card key={index} style={{ width: '6rem', marginBottom: '10px', border: 'none' }}>
+//                       <Image src={value} className="left-img-style" />
+//                     </Card>
+//                   ))
+//                 }
+//               </Col>
+//             </div>
+//             <div className="main-img">
+//               <Col>
+//                 <Image className="img-style" src={details1.img} />
+//               </Col>
+//             </div>
+//             <Col className="right-details">
+//               <div className="right-box">
+//                 <h3>{details1.name}</h3>
+//                 <h5 className="name1">{details1.name1}</h5>
+//                 <p className="rating">
+//                   <button>{details1.rating}<span>/{details1.view}</span> Rating</button>
+//                 </p>
+//                 <h4>&#8377;{details1.newPrice}</h4>
+//                 <p className="mrp">MRP <del>&#8377;{details1.oldPrice}</del> {details1.offer}</p>
+//                 <p>Price inclusive of all taxes</p>
+//                 <p className="size-section"><h5>Select Size</h5></p>
+//                 <div className="size-btn">
+//                   {
+//                     details1 && details1.size.map((value, index) => (
+//                       <button 
+//                         key={index} 
+//                         className={`size-btn1 ${selectedSize === value ? 'selected' : ''}`} 
+//                         onClick={() => setSelectedSize(value)}
+//                       >
+//                         {value}
+//                       </button>
+//                     ))
+//                   }
+//                 </div>
+//                 <h5 className="text-color">Color Choices</h5>
+//                 <div className="color-btn">
+//                   {
+//                     details1 && details1.color.map((color, index) => (
+//                       <button 
+//                         key={index} 
+//                         style={{ backgroundColor: color, width: 30, height: 30, marginLeft: 10, border: selectedColor === color ? '2px solid black' : 'none' }}
+//                         onClick={() => setSelectedColor(color)}
+//                       >
+//                       </button>
+//                     ))
+//                   }
+//                 </div>
+//                 <div className="buy-add">
+//                   <button type="button" onClick={BuyNow}>Buy Now</button>
+//                   <button type="button" onClick={AddTo}>Add to Cart</button>
+//                 </div>
+//               </div>
+//             </Col>
+//           </div>
+//         </Row>
+//       </Container>
+//     </>
+//   )
+// }
+
+// export default ProductDetails;
 
 
- 
-function ProductDetails(){
 
 
 
-  
-  // ---------Men Data Coming-----------------------------------------------------
+import { Card, Container, Image, Col, Row } from "react-bootstrap";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import '../style/AddToCart.css';
 
-    const loc = useLocation('')
-    const navigation = useNavigate('')
+function ProductDetails() {
+  const loc = useLocation();
+  const navigate = useNavigate();
+  const [details1, setDetails] = useState('');
+  const [selectedSize, setSelectedSize] = useState('');
+  const [selectedColor, setSelectedColor] = useState('');
 
-    const [detaisl1,setDetails]=useState('')
+  useEffect(() => {
+    setDetails(loc.state);
+  }, [loc.state]);
 
-    useEffect(()=>{
-        // console.log(loc.state);
-        setDetails(loc.state);
-     
-    },[])
+  function AddTo() {
+    navigate('/AddToCart', { state: { ...details1, selectedSize, selectedColor } });
+  }
 
+  const BuyNow = () => {
+    navigate('/AddToCart', { state: { ...details1, selectedSize, selectedColor } });
+  }
 
-    function AddTo(){
-     navigation('/AddToCart',detaisl1);
-    }
-
-    const BuyNow=()=>{
-      navigation('/AddToCart  ')
-    }
-    return(
-        <>
-       <Container >
+  return (
+    <>
+      <Container>
         <div className="heading">
-
-        <h1 className="heading-add">Product Details </h1>
-        <hr className="heading-line" />
+          <h1 className="heading-add">Product Details</h1>
+          <hr className="heading-line" />
         </div>
-       <Row>
-        <div className="image-section">
-
-        <div className="left-img">
-
-       <Col>
-
-        {
-            detaisl1&&detaisl1.sideimg.map(value=>(
-                <Card style={{width:'6rem',marginBottom:'10px',border:'none'}}>
-                  <Image src={value} className="left-img-style"/>
-                  
-                </Card>
-            ))
-        }
-     
-        
-          </Col>
-        </div>
-            <div className="main-img">
-
-          <Col>
-            <Image className="img-style" src={detaisl1.img} ></Image>
-          
-          </Col>
+        <Row>
+          <Col md={4} className="image-section">
+            <div className="left-img">
+              {
+                details1 && details1.sideimg.map((value, index) => (
+                  <Card key={index} style={{ width: '6rem', marginBottom: '10px', border: 'none' }}>
+                    <Image src={value} className="left-img-style" />
+                  </Card>
+                ))
+              }
             </div>
-        
-       
-
-            <Col className="right-details">
+          </Col>
+          <Col md={4} className="main-img">
+            <Image className="img-style" src={details1.img} fluid />
+          </Col>
+          <Col md={4} className="right-details">
             <div className="right-box">
-
-                <h3>{detaisl1.name}</h3>
-                <h5 className="name1">{detaisl1.name1}</h5>
-                <p className="rating"> <button>{detaisl1.rating}<span>/{detaisl1.view}</span> Rating</button></p>
-                <h4>&#8377;{detaisl1.newPrice}</h4>
-                <p className="mrp">MRP <del>&#8377;{detaisl1.oldPrice}</del> {detaisl1.offer}</p>
-                <p>Price inclusive of all taxes</p>
-
-                <p className="size-section"><h5>Select Size</h5></p>
-                <div className="size-btn">
-
-               {
-                 detaisl1&&detaisl1.size.map(value=>(
-                   <button className="size-btn1">{value}</button>
-                  ))
-                }
-                </div>
-               <h5 className="text-color">Color Chooses</h5>
-               <div className="color-btn">
-
+              <h3>{details1.name}</h3>
+              <h5 className="name1">{details1.name1}</h5>
+              <p className="rating">
+                <button>{details1.rating}<span>/{details1.view}</span> Rating</button>
+              </p>
+              <h4>&#8377;{details1.newPrice}</h4>
+              <p className="mrp">MRP <del>&#8377;{details1.oldPrice}</del> {details1.offer}</p>
+              <p>Price inclusive of all taxes</p>
+              <p className="size-section"><h5>Select Size</h5></p>
+              <div className="size-btn">
                 {
-                  detaisl1&&detaisl1.color.map(color=>(
-                    <button style={{backgroundColor:`${color}` , width:30, height:30 , marginLeft:10}}></button>
+                  details1 && details1.size.map((value, index) => (
+                    <button 
+                      key={index} 
+                      className={`size-btn1 ${selectedSize === value ? 'selected' : ''}`} 
+                      onClick={() => setSelectedSize(value)}
+                    >
+                      {value}
+                    </button>
                   ))
                 }
-              
-                </div>
-                <div className="buy-add">
-               
-                    <button type="button" onClick={BuyNow} > Buy Now</button>
-                    <button type="button" onClick={AddTo}> Add to Cart</button>
-                
-                </div>
-                   
-                </div>
-            </Col>
-                </div>
-        </Row> 
-       
-       </Container>
-        </>
-    )
+              </div>
+              <h5 className="text-color">Color Choices</h5>
+              <div className="color-btn">
+                {
+                  details1 && details1.color.map((color, index) => (
+                    <button 
+                      key={index} 
+                      style={{ backgroundColor: color, width: 30, height: 30, marginLeft: 10, border: selectedColor === color ? '2px solid black' : 'none' }}
+                      onClick={() => setSelectedColor(color)}
+                    >
+                    </button>
+                  ))
+                }
+              </div>
+              <div className="buy-add">
+                <button type="button" onClick={BuyNow}>Buy Now</button>
+                <button type="button" onClick={AddTo}>Add to Cart</button>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
 }
-export default ProductDetails
+
+export default ProductDetails;
